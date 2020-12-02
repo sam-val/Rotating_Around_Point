570d864cb86db6b78c206ad3c3745070ea9e8cb4
Sam Val <samvalentine2811@gmail.com>
2020-12-02 22:47:55 +0700

    Rotation feature

--- main.py
+++ main.py
@@ -2,42 +2,49 @@ from PyQt5.Qt import *
 import sys
 from collections import deque
 
+# Colours:
+EMPTY = 'gray'
+CENTER = 'green'
+BODY = 'red'
+
 class Cube(QWidget):
     clicked = pyqtSignal()
+    right_clicked = pyqtSignal()
     def __init__(self, width, x, y):
         super().__init__()
         self.width = width
         self.setFixedSize(self.width, self.width)
         self.setAutoFillBackground(True)
-        self.colour = 'gray'
+        self.colour = EMPTY
         self.pos = x, y
         self.states = deque([0,1,2])
 
         self.changeColour(self.colour)
 
     def mousePressEvent(self, e) -> None:
-        # print(self.x, self.y)
-        self.clicked.emit()
-        # self.changeColour('red')
+        if e.button() == Qt.LeftButton:
+            self.clicked.emit()
+        if e.button() == Qt.RightButton:
+            self.right_clicked.emit()
 
     def changeColour(self, colour):
         pallete = self.palette()
         pallete.setColor(QPalette.Window, QColor(colour))
         self.setPalette(pallete)
 
-
-
-
 class MyWin(QMainWindow):
-    def __init__(self, cube_width=30):
+    def __init__(self,  width, height, cube_width=30):
         super().__init__()
         self.cube_w = cube_width
+        self.width = width
+        self.height = height
         # Layout -- making the grid
         self.grid = QGridLayout()
         self.grid.setSpacing(2)
-        self.make_grid(4,4)
+        self.make_grid(self.width,self.height)
         self.center_cube = None
         self.body = set()
+        self.rot_direction = False # true is clockwise, false is counter clockwise
 
         # Create a dummy widget
         dummy_widget = QWidget()
@@ -53,6 +60,47 @@ class MyWin(QMainWindow):
                 cube = Cube(self.cube_w, x, y)
                 self.grid.addWidget(cube, x, y, 1, 1)
                 cube.clicked.connect(self.cube_is_clicked)
+                cube.right_clicked.connect(self.rotateShape)
+
+    def rotateShape(self):
+        if self.center_cube:
+            # make rotation matrix:
+            trans_matrix = [[0, -1],
+                            [1,  0]] if self.rot_direction else [[0 , 1],
+                                                                 [-1, 0]]
+            new_body = []
+
+            # make a new shape (list that contains cube pos)
+            for x,y in self.body:
+                center_x, center_y = self.center_cube.pos
+                # find relative vector (pointing from center cube to the current cube
+                rel_x, rel_y = center_x - x, center_y - y
+                # compute new relative vector:
+                new_rel_x = rel_x * trans_matrix[0][0] + rel_y * trans_matrix[0][1]
+                new_rel_y = rel_x * trans_matrix[1][0] + rel_y * trans_matrix[1][1]
+                # compute relative back to normal vector(that means it starts from 0,0)
+                # which is the sum of relative vec and vector of the center:
+                new_pos = new_rel_x + center_x, new_rel_y + center_y
+                new_body.append(new_pos)
+
+            self.body = set(new_body)
+
+            # then redraw it on screen:
+            for y in range(self.height):
+                for x in range(self.width):
+                    cube = self.grid.itemAtPosition(x, y).widget()
+                    if (x, y) == self.center_cube.pos:
+                        cube.changeColour(CENTER)
+                    elif (x,y) not in self.body:
+                        cube.changeColour(EMPTY)
+                    else:
+                        cube.changeColour(BODY)
+        else:
+            print("No center cube selected")
+
+    def changeRotationDirection(self):
+        self.rot_direction = not self.rot_direction
+        print("Rotation:","Clockwise" if self.rot_direction else "Counter Clockwise" )
 
     def cube_is_clicked(self):
         cube = self.sender()
@@ -67,38 +115,33 @@ class MyWin(QMainWindow):
                 self.body.remove(pos)
             elif cube == self.center_cube:
                 self.center_cube = None
-            cube.changeColour('gray')
+            cube.changeColour(EMPTY)
 
         elif next_state == 1:
             if pos not in self.body:
                 self.body.add(pos)
-            cube.changeColour("red")
+            cube.changeColour(BODY)
 
         elif next_state == 2:
             if self.center_cube != None:
                 # is there is already a center cube, grab it:
                 current_cen_x, current_cen_y = self.center_cube.pos
                 cen_cube = self.grid.itemAtPosition(current_cen_x, current_cen_y).widget()
-                print(type(cen_cube))
 
-                # change state of current center cube to 1, add it to the body and chnage it's colour:
-                self.changeState(cen_cube, at=1)
-                self.body.add(cen_cube.pos)
-                cen_cube.changeColour('red')
+                # change state of current center cube to 0 and chnage it's colour:
+                self.changeState(cen_cube, at=0)
+                cen_cube.changeColour(EMPTY)
 
                 # and set the current cube to the center, and it's colour:
                 self.center_cube = cube
-                cube.changeColour('green')
+                cube.changeColour(CENTER)
             else:
                 self.center_cube = cube
-                cube.changeColour("green")
+                cube.changeColour(CENTER)
 
             # now it is the center cube, remove it from the body:
             if pos in self.body:
                 self.body.remove(pos)
-
-        print('center cube pos is', f'{self.center_cube.pos}' if self.center_cube is not None else 'None')
-        print(f'body: {self.body}')
 
     def setCenterCube(self, cube):
         pass
@@ -117,14 +160,22 @@ class MyWin(QMainWindow):
         key = e.key()
         if key == Qt.Key_Q:
             self.close_win()
+        elif key == Qt.Key_R:
+            self.changeRotationDirection()
 
     def close_win(self):
         print('closing app...')
         self.close()
 
-CUBE_WIDTH = 100
+CUBE_WIDTH = 120
 app = QApplication(sys.argv)
-win = MyWin(CUBE_WIDTH)
+win = MyWin(6,6,CUBE_WIDTH)
+
+
+## align cetner
+new_rect = QStyle.alignedRect(Qt.LeftToRight, Qt.AlignCenter, win.size(),
+                              app.desktop().availableGeometry())
+win.setGeometry(new_rect)
 
 sys.exit(app.exec_())
 
